12c2688327dd67dcda60c213aa8efc8f29f5f9d0
wisdomokereke <159027943+wisdomokereke@users.noreply.github.com>
2025-12-09 09:27:34 +0100

    Added 2 extra packages
    npm install --save-dev @types/react @types/react-dom

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,12 +1,11 @@
+
 function Message() {
 
     const name = "Emma";
     
     return (
-    <>  
-     <h1>Hello, {name}!</h1>
-     <h1>Welcome to the Message Component</h1>
-    </>
+        <h1>Hello, {name}!</h1>
+        <h1>Welcome to the Message Component</h1>
     );
 }
 
